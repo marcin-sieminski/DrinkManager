8a1e2ee44d7c90f3aaf6530fba30857ca8263131
Marek <wilkmarek0@gmail.com>
2020-10-24 22:43:03 +0200

    Added missing part of the code

--- Tests/DrinkFavouriteServiceShould.cs
+++ Tests/DrinkFavouriteServiceShould.cs
@@ -1,6 +1,8 @@
-﻿using BLL.Data.Repositories;
+﻿using BLL;
 using DrinkManagerWeb.Services;
 using FluentAssertions;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -11,7 +13,7 @@ namespace Tests
         [Fact]
         public void ChangeIsFavouriteToTrueIfMakeItFavouriteWasCalled()
         {
-            var list = new TestDrinkRepository().GetTestDrinks();
+            var list = new SampleDrinkCreator().GetTestDrinks();
             var favouriteService = new DrinkFavouriteService();
             
             favouriteService.MakeItFavourite(list.First());
@@ -21,7 +23,7 @@ namespace Tests
         [Fact]
         public void ChangeIsFavouriteToFalseIfMakeItNotFavouriteWasCalled()
         {
-            var list = new TestDrinkRepository().GetTestDrinks();
+            var list = new SampleDrinkCreator().GetTestDrinks();
             var favouriteService = new DrinkFavouriteService();
             favouriteService.MakeItFavourite(list.First());
 
@@ -29,6 +31,87 @@ namespace Tests
 
             list.First().IsFavourite.Should().Be(false);
 
+        }
+    }
+
+    public class SampleDrinkCreator
+    {
+        public List<Drink> GetTestDrinks()
+        {
+            var drinkList = new List<Drink>()
+            {
+                new Drink()
+                {
+                    AlcoholicInfo = "Alcoholic",
+                    Category = "Special",
+                    DrinkId = "1",
+                    DrinkReview = new DrinkReview()
+                    {
+                        Id = 1,
+                        ReviewDate = DateTime.Now,
+                        ReviewScore = 4,
+                        ReviewText = "Cool"
+                    },
+                    GlassType = "Regular",
+                    ImageUrl = "",
+                    Ingredients = new List<Ingredient>()
+                    {
+                        new Ingredient()
+                        {
+                            Amount = "300ml",
+                            DrinkId = "1",
+                            IngredientId = "1",
+                            Name = "Piccolo"
+                        },
+                        new Ingredient()
+                        {
+                            Amount = "Until Full",
+                            DrinkId = "1",
+                            IngredientId = "2",
+                            Name = "Vodka"
+                        }
+                    },
+                    Instructions = "Stir stuff",
+                    IsFavourite = false,
+                    Name = "Turbo Piccolo"
+                },
+                new Drink()
+                {
+                    AlcoholicInfo = "Non Alcoholic",
+                    Category = "Not special",
+                    DrinkId = "2",
+                    DrinkReview = new DrinkReview()
+                    {
+                        Id = 2,
+                        ReviewDate = DateTime.Now,
+                        ReviewScore = 2,
+                        ReviewText = "Nah"
+                    },
+                    GlassType = "Irregular",
+                    ImageUrl = "",
+                    Ingredients = new List<Ingredient>()
+                    {
+                        new Ingredient()
+                        {
+                            Amount = "200ml",
+                            DrinkId = "2",
+                            IngredientId = "3",
+                            Name = "Rum"
+                        },
+                        new Ingredient()
+                        {
+                            Amount = "Until Full",
+                            DrinkId = "2",
+                            IngredientId = "2",
+                            Name = "Vodka"
+                        }
+                    },
+                    Instructions = "Stir stuff",
+                    IsFavourite = false,
+                    Name = "Turbo Rum"
+                }
+            };
+            return drinkList;
         }
     }
 }
